08cde4772bc383c48e1d3c97322f9d15cd29a6e1
Klaus <yinshikai@xuningtech.com>
2015-09-08 16:45:16 +0800

    login

--- public/js/src/home/app.js
+++ public/js/src/home/app.js
@@ -17,8 +17,10 @@ requirejs.config({
 	waitSeconds: 30
 });
 
-requirejs(['react', 'jquery', 'WQ', 'home/home/home'],function(React, $, WQ, Home){
+requirejs(['react', 'jquery', 'WQ', 'home/home/home', 'home/login/signin'],function(React, $, WQ, Home, Signin){
 
-    React.render(<Home />, $('#container').get(0));
+    // React.render(<Home />, $('#container').get(0));
+
+    React.render(<Signin />, $('#login').get(0));
     
 })
---
+++ public/js/src/home/login/signIn.js
@@ -0,0 +1,16 @@
+define(['react','jquery'],function(React, $) {
+
+  return  React.createClass({
+
+    render: function() {
+      return (
+        <div className="commentBox">
+          eeeeeeee<br>
+          <p>fsdafsda</p>
+        </div>
+      );
+    }
+
+  });
+
+})
