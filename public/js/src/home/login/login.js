9ff794bd6c88ce77f9204e3f7d63320b312dcd28
2409551912 <2409551912@qq.com>
2015-09-08 13:44:24 +0800

    add login

---
+++ public/js/src/home/login/login.js
@@ -0,0 +1,15 @@
+define(['react','jquery'],function(React, $) {
+var CommentBox = React.creatClass({
+		render:function(){
+		return(
+		<div class="sign-in">
+			gfdsghsdfhf
+		</div>
+		)
+		}
+	});
+	React.render(
+		<CommentBox />,
+		document.getElementById("content")
+	)
+})
