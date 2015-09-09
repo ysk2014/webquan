30da4cf14bf0219e455c30e2ccf8b823a3211dea
2409551912 <2409551912@qq.com>
2015-09-09 23:31:52 +0800

    update signIn

--- public/js/src/home/login/signIn.js
+++ public/js/src/home/login/signIn.js
@@ -1,19 +1,55 @@
 define(['react','jquery'],function(React, $) {
 
+    var SignUpForm = React.createClass({
+        render: function(){
+        return(
+            <form method="post">
+                <input type="text" name= "username" placeholder="请输入手机号或邮箱" />
+                <div className="code">
+                    <input type="text" name="code" placeholder="短信验证码" />
+                    <a href="#" className="gain" >点击获取</a>
+                    <div className="clear"></div>
+                </div>
+                <input type="text" name="password" placeholder= "请输入密码" />
+                <input type="submit" className="sub" value="注册" />
+            </form>
+        )    
+        }
+    });
+
+    var SignInForm = React.createClass({
+        render: function(){
+        return(
+            <form method="post">
+                <input type="text" name="username" placeholder="请输入手机号或邮箱" />
+                <input type="text" name="password" placeholder="请输入密码" />
+                <input type="submit" className="sub" value="登录" />
+                <a className="text-left" href="">忘记密码</a>
+            </form>
+        )    
+        }
+    });
+    var Choose = React.createClass({
+        render: function(){
+        return(
+            <div className="choose">
+                <span><a href="#">登陆</a><a href="#">注册</a></span>
+            </div>
+        )    
+        }
+    });
+
     return React.createClass({
         getInitialState: function() {
             return {
-                name: 'signIn',
+                name: 'LoginIn',
             }
         },
         render: function() {
             return (
                 <div className="LoginBox" id="content">
-                    <form method="post">
-                        <input type = "text" name = "username" placeholder = "请输入手机号或邮箱"/>
-                        <input type="submit" className = "sub" value="登陆/注册" />
-                        <a className = "text-left" href = "">忘记密码</a>
-                    </form>
+                    <Choose />
+                    <SignInForm />
                 </div>
             );
         }
