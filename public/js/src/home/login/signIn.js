2f5db374f9da0a6cda61b6fcfea069e76c177764
Klaus <yinshikai@xuningtech.com>
2015-09-09 13:12:40 +0800

    signIn

--- public/js/src/home/login/signIn.js
+++ public/js/src/home/login/signIn.js
@@ -7,26 +7,26 @@ define(['react','jquery'],function(React, $) {
             }
         },
         render: function() {
-        return (
-            <div className="LoginBox" id="content">
-                <form className = "form-horizontal" method="post">
-                    <div className = "input-prepend">
-                        <span className = "add-on">
-                            <i className = "icon-user"></i>
-                        </span>
-                        <input type = "text" name = "username" placeholder = "用户名"/>
-                    </div>
-                    <div className = "input-prepend">
-                        <span className = "add-on">
-                            <i className = "icon-key"></i>
-                        </span>
-                        <input type = "password" name = "password" placeholder = "密码"/>
-                    </div>
-                    <input type="submit" className = "btn btn-submit" />
-                    <a className = "text-right" href = "">忘记密码</a>
-                </form>
-            </div>
-        );
+            return (
+                <div className="LoginBox" id="content">
+                    <form className = "form-horizontal" method="post">
+                        <div className = "input-prepend">
+                            <span className = "add-on">
+                                <i className = "icon-user"></i>
+                            </span>
+                            <input type = "text" name = "username" placeholder = "用户名"/>
+                        </div>
+                        <div className = "input-prepend">
+                            <span className = "add-on">
+                                <i className = "icon-key"></i>
+                            </span>
+                            <input type = "password" name = "password" placeholder = "密码"/>
+                        </div>
+                        <input type="submit" className = "btn btn-submit" />
+                        <a className = "text-right" href = "">忘记密码</a>
+                    </form>
+                </div>
+            );
         }
     });
 })
