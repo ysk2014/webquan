1a5614e62b336ce9e43c927f6245a61c862b73b2
Klaus <yinshikai@xuningtech.com>
2015-10-28 17:44:47 +0800

    bug

--- config/database.php
+++ config/database.php
@@ -116,7 +116,7 @@ return [
 
         'default' => [
             'host'     => '127.0.0.1',
-            'port'     => 5000,
+            'port'     => 6379,
             'database' => 0,
         ],
 
