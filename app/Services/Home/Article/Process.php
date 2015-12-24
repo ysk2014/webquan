18e676be91a2b975e8a12f787f53d7438bde7dc5
ysk2014 <1181102772@qq.com>
2015-12-24 22:21:14 +0800

    bug

--- app/Services/Home/Article/Process.php
+++ app/Services/Home/Article/Process.php
@@ -536,7 +536,7 @@ class Process extends BaseProcess
 		$articleInfo = $this->userArticleModel->getArticlesByUid($data['uid'],$page,$type);
 		if($articleInfo) {
 
-			$count = $this->userArticleModel->getCount($data['uid'],$type);
+			$count = $this->userArticleModel->getArtCount($data['uid'],$type);
 			if( (intval($data['page'])+1)*20 < $count ) {
 				$next = true;
 			} else {
