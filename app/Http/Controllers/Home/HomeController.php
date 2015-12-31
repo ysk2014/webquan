8d1024fb2e890a1ce2a3a3caa9f9da5ac28f7c9f
ysk2014 <1181102772@qq.com>
2015-12-31 18:21:23 +0800

    update

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{	
-		return view('home/app');
+		return view('home/home', ['title' => 'Samantha']);
 	}
 
 }
---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,8 @@
+
+@extends('home.app')
+
+@section('title', $title)
+
+@section('content')
+	<a class="btn btn-primary" href="#">Link</a>
+@endsection
