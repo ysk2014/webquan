e7cae0446f5193428702a8d782d93a61ca0fac8e
ysk2014 <1181102772@qq.com>
2016-01-01 21:15:50 +0800

    bug

---
+++ resources/views/home/article/editArticle.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta property="qc:admins" content="15413750205567165526367" />
+	<meta property="wb:webmaster" content="8cde258978bf6c9f" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="keywords" content="Web圈, 前端, 前端开发, web前端, web前端开发, 前端开发工程师, 前端开发攻城师,设计, 开发, 前端资源, CSS, JavaScript, Ajax, jQuery, html,html5,css3,浏览器兼容, 前端开发工具, 前端招聘, jQuery API, CSS整形和优化工具 , JS压缩工具 , JS格式化工具 , CSS参考手册 ,HTML参考手册 , 多重搜索 , nodejs , node , boostrap , sublime">
+	<meta name="description" content="Web圈，这里是最专业的Web开发知识平台，最精彩的原创文章专栏，最专业的问答平台，最火热的话题频道，最丰富的线下线上活动。html,js,css,nodejs,前端开发,前端招聘,jquery,qianduan">
+	<title>@yield('title')</title>
+	<link href="{{ asset('image/web.ico') }}" rel="shortcut icon">
+	<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/web-ui-light.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<script type="text/javascript" src="{{ asset('js/lib/jquery-1.11.3.min.js') }}"></script>
+</head>
+
+<body>
+	<div class="navbar navbar-default" id="header">
+		<div class="container">
+			<a class="logo" href="/">
+				<img alt="web圈" src="{{ asset('image/logo.png') }}">
+			</a>
+			<a class="navbar-toggle collapsed" type="button" data-toggle="collapse" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		    </a>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="">首页</a></li>
+					<li><a href="">问答</a></li>
+					<li><a href="">文章</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="search"><a href=""><i class="fa fa-search"></i></a></li>
+					<li class="login"><a href="">登录/注册</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+    <div class="container" id="main">
+    	<div class="row">
+    		<div class="col-md-8">@yield('content')</div>
+    		<div class="col-md-4 sidebar">@yield('aside')</div>
+    	</div>
+    </div>
+</body>
+</html>
