bb201d8dfed257441b6e42efc5959cb5cd84695c
ysk2014 <1181102772@qq.com>
2016-04-01 17:26:32 +0800

    bug

--- resources/views/home/user/news.blade.php
+++ resources/views/home/user/news.blade.php
@@ -6,15 +6,17 @@
 <div class="container" id="main">
 	<div class="row">
     	<div class="col-md-8">
-    		@if ($news['rc']==0)
-				<ul class="list-group">
-					@foreach ($news['data'] as $new)
-						<li class="list-group-item"><?php echo $new['content']; ?></li>
-					@endforeach
-				</ul>
-			@else
-				没有消息
-			@endif
+    		<div class="news-page">
+    			@if ($news['rc']==0)
+					<ul class="list-group">
+						@foreach ($news['data'] as $new)
+							<li class="list-group-item"><?php echo $new['content']; ?></li>
+						@endforeach
+					</ul>
+				@else
+					没有消息
+				@endif
+    		</div>
     	</div>
 
     	<div class="col-md-4 sidebar">
